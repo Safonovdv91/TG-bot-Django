e968aa3d6764f38eeb9b0004bc8dfbd02faf58ca
Safonov <Sofik.d@gmail.com>
2025-04-23 15:52:37 +0300

    ADD: Создан скелет шаблонов для страницы

--- core/index/views.py
+++ core/index/views.py
@@ -1,8 +1,11 @@
+from django.http import HttpRequest
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-def index(request):
+@login_required(login_url="login")
+def index(request: HttpRequest):
     context = {
         "letters": ["A", "B", "C1", "C2", "C3", "D1", "D2", "D3", "D4", "N"],
     }
