58a4cf8905e9e2138f950c98bd9eab7e40ed69fc
Safonov <Sofik.d@gmail.com>
2025-04-24 16:03:10 +0300

    ADD: Добавлены модели и связь M2M для подписок пользователя на классы - типы соревнований

--- core/gymkhanagp/admin.py
+++ core/gymkhanagp/admin.py
@@ -1,2 +1,27 @@
+from django.contrib import admin
+from .models import (
+    CompetitionTypeModel,
+    SportsmanClassModel,
+    Subscription,
+    UserSubscription,
+)
 
-# Register your models here.
+
+@admin.register(CompetitionTypeModel)
+class CompetitionTypeAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+
+
+@admin.register(SportsmanClassModel)
+class SportsmanClassAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("user_subscription", "competition_type", "sportsman_class")
+
+
+@admin.register(UserSubscription)
+class UserSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("user", "created_at", "is_active")
