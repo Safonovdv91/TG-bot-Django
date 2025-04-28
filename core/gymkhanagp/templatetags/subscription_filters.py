19352e94201cf593565afe9b5a10a65b64af28b4
Safonov <Sofik.d@gmail.com>
2025-04-28 13:09:21 +0300

    bugfix: фильтр с отображением показывает только для GGP соревнований класс

--- core/gymkhanagp/templatetags/subscription_filters.py
+++ core/gymkhanagp/templatetags/subscription_filters.py
@@ -5,12 +5,22 @@ register = template.Library()
 
 @register.filter(name="is_subscribed")
 def is_subscribed(competition_type, user_subscriptions):
-    "Фильтр проверяющий подписан ли пользователь на данный тип соревнований"
+    "Фильтр проверяющий подписан ли пользователь на данный тип соревнований, усли хоть один тип то True"
     return any(ct_id == competition_type.id for ct_id, _ in user_subscriptions)
 
 
 @register.filter(name="is_subscribed_class")
 def is_subscribed_class(sport_class, user_subscriptions):
-    "Фильтр проверяющий подписан ли пользователь на данный класс спортсмена"
+    """
+    Фильтр проверяющий подписан ли пользователь на данный класс.
+    В данный момент проверка проводится для ggp, поэтому в запросе отправляется запрос с параметром competition_type=1
 
-    return any(sc_id_ == sport_class.id for _, sc_id_ in user_subscriptions)
+    :param sport_class:
+    :param user_subscriptions:
+    :return:
+    """
+    competition_type_id = 1
+    for sc_id_, _ in user_subscriptions:
+        if sc_id_ == sport_class.id and _ == competition_type_id:
+            return True
+    return False
