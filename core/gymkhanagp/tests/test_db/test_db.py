26c52c8779b1c92df53b85beec29a4ada31ed505
DSafonov-home <Softik.d@gmail.com>
2025-04-29 22:10:23 +0300

    ref

--- core/gymkhanagp/tests/test_db/test_db.py
+++ core/gymkhanagp/tests/test_db/test_db.py
@@ -2,8 +2,7 @@ from django.contrib.auth.models import User
 import pytest
 
 @pytest.mark.django_db
-def test_user_create():
-    User.objects.create_user("test", "test@tmas.ir", "test")
+def test_user_create(user):
     count = User.objects.all().count()
     assert count == 1
     
@@ -12,5 +11,10 @@ def test_user_not_create():
     count = User.objects.all().count()
     assert count == 0
     
-def test_change_password(user_1):
+def test_username(user_1):
     assert user_1.username == "test_user"
+    
+    
+def test_change_password(user_1):
+    user_1.set_password("new_password")
+    assert user_1.check_password("new_password") == True
