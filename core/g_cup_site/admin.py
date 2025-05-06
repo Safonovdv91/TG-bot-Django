f6bac34cb9c16a1de778711b5f4497ff20d4a1ed
Safonov <Sofik.d@gmail.com>
2025-05-06 10:55:41 +0300

    ruff

--- core/g_cup_site/admin.py
+++ core/g_cup_site/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import *
+from .models import (
+    ChampionshipModel,
+    StageModel,
+    MotorcycleModel,
+    AthleteModel,
+    StageResultModel,
+)
 
 
 @admin.register(ChampionshipModel)
