5d8adc0ed2c339630517b74abcfcc447949e8876
Safonov <Sofik.d@gmail.com>
2025-05-12 12:57:13 +0300

    bugfix:

--- core/users/utils.py
+++ core/users/utils.py
@@ -1,7 +1,7 @@
 from allauth.socialaccount.models import SocialAccount
 
 
-def get_telegram_id(user) -> SocialAccount | None:
+def get_telegram_id(user) -> int | None:
     try:
         social_account = SocialAccount.objects.get(user=user, provider="telegram")
         return social_account.extra_data.get("id")
