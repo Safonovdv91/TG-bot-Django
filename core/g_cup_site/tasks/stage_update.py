6a4f2743ef0b6ed83bf79b53a25c2c8fac896c83
Safonov <Sofik.d@gmail.com>
2025-05-14 15:28:34 +0300

    ruff:

--- core/g_cup_site/tasks/stage_update.py
+++ core/g_cup_site/tasks/stage_update.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 
 @celery_app.task
-def stage_update(stage_id:int):
+def stage_update(stage_id: int):
     """Периодическое обновление этапа c переданным id."""
     stage_results = StageGGPHandeler(stage_id)
     stage_results.handle()
