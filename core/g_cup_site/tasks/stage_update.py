2e039b1498723ab1b1c2cb5b5be3ea2f1de50383
dsafonov <Softik.d@gmail.com>
2025-05-18 20:51:23 +0300

    feat: Обновление данных для атлетов в БД( для поддержания консистентности с сайтом)

--- core/g_cup_site/tasks/stage_update.py
+++ core/g_cup_site/tasks/stage_update.py
@@ -1,7 +1,10 @@
 import logging
 
+from django.db import transaction
+
 from core import celery_app
-from g_cup_site.utils import StageGGPHandeler, BaseFigureHandler
+from g_cup_site.models import AthleteModel
+from g_cup_site.utils import StageGGPHandeler, BaseFigureHandler, APIGetter
 
 logger = logging.getLogger(__name__)
 
@@ -18,3 +21,24 @@ def base_figure_update(figure_id: int):
     """Получение результатов для базовой фигуры."""
     stage_results = BaseFigureHandler(figure_id)
     stage_results.handle()
+
+
+def update_all_athletes_info():
+    api = APIGetter()
+    count = 0
+    with transaction.atomic():
+        athletes: list[AthleteModel] = AthleteModel.objects.all()
+        for athlete in athletes:
+            fresh_athlete_data = api.get_athlete_data(athlete.id)
+
+            if athlete.sportsman_class != fresh_athlete_data.get("class"):
+                count += 1
+
+            athlete.first_name = fresh_athlete_data.get("firstName")
+            athlete.last_name = fresh_athlete_data.get("lastName")
+            athlete.sportsman_class = fresh_athlete_data.get("class")
+            athlete.img_url = fresh_athlete_data.get("imgUrl")
+            athlete.number = fresh_athlete_data.get("number")
+
+            athlete.save()
+    return f"Обновили класс {count} спортсменов из {len(athletes)}."
