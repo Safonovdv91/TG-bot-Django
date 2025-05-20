a57a404de4ff8df4583cff462f14c8588f4ec527
Safonov <Sofik.d@gmail.com>
2025-05-20 14:11:39 +0300

    fix: DRY

--- core/g_cup_site/management/commands/update_athelete_classes.py
+++ core/g_cup_site/management/commands/update_athelete_classes.py
@@ -2,6 +2,8 @@ import logging
 
 from django.core.management.base import BaseCommand
 from django.db import transaction
+
+from g_cup_site.tasks import update_all_athletes_info
 from g_cup_site.utils import APIGetter
 from g_cup_site.models import (
     AthleteModel,
@@ -14,27 +16,4 @@ class Command(BaseCommand):
     help = "Обновление данных о спортсменах в базе данных"
 
     def handle(self, *args, **options):
-        api = APIGetter()
-        logger.info("Начинаем получение данных...")
-        try:
-            with transaction.atomic():
-                athletes: list[AthleteModel] = AthleteModel.objects.all()
-                print(f"Всего спортсменов: {len(athletes)}")
-                for athlete in athletes:
-                    print(f"Обновляем спортсмена: {athlete}")
-                    fresh_athlete_data = api.get_athlete_data(athlete.id)
-                    athlete.first_name = fresh_athlete_data.get("firstName")
-                    athlete.last_name = fresh_athlete_data.get("lastName")
-                    athlete.sportsman_class = fresh_athlete_data.get("class")
-                    athlete.img_url = fresh_athlete_data.get("imgUrl")
-                    athlete.number = fresh_athlete_data.get("number")
-
-                    athlete.save()
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        "Data import championships completed successfully!"
-                    )
-                )
-        except Exception as e:
-            self.stdout.write(self.style.ERROR(f"Error during import: {str(e)}"))
-            raise
+        update_all_athletes_info()
