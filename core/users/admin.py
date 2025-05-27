b0a76989cc1a03cb2d696cf373c704a0347ff4a7
Safonov <Sofik.d@gmail.com>
2025-05-27 15:02:59 +0300

    feat: Добавлено в админ панеле отображение отчетов

--- core/users/admin.py
+++ core/users/admin.py
@@ -5,5 +5,10 @@ from .models import Report
 
 @admin.register(Report)
 class ReportAdmin(admin.ModelAdmin):
-    list_display = ("user", "created_at", "source", "text")
+    list_display = ("text", "user", "created_at","updated_at", "source", "report_type","resolved")
     list_display_links = ("user",)
+    ordering = ("report_type", "-created_at")
+    list_filter = ("resolved", "report_type")
+
+    list_per_page = 30
+
