a4c7ef6d6f03458ffc96ca26ee38c101f24dee94
Safonov <Sofik.d@gmail.com>
2025-05-28 16:00:33 +0300

    feat: добавлены команды обработки отчета / бага

--- core/telegram_bot/commands.py
+++ core/telegram_bot/commands.py
@@ -25,7 +25,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None | St
         можно получать уведомления о: GGP и Базовые фигуры. Там выбираешь класс - и будешь получать 
         уведомления о всех результатах этого класса.
         Так же если найдешь баг то либо пиши @@SoftikMy или пришли команду /bug_report.
-        Если есть крутые идеи по расширению возможностей то пришли команду /feature и комаанда разработки
+        Если есть крутые идеи по расширению возможностей то пришли команду /feature и команда разработки
         приступит к реализации твоей идеи!
         """
         logger.info(f"New user: {user}")
@@ -54,14 +54,9 @@ async def bug_report(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Stat
         context.user_data["state"] = States.BUG_REPORT_WAIT
         return States.BUG_REPORT_WAIT
 
-    user = get_user_by_telegram_id(update.effective_user.id)
-    success, message = await ReportHandler.handle_report(
-        user=user, text=text, source=SourceReports.TELEGRAM, type_report=TypeReport.BUG
+    message = await register_report(
+        telegram_id=update.effective_user.id, text=text, type_report=TypeReport.BUG
     )
-
-    if not success:
-        admin_contact = await AdminNotifier.get_admin_contacts()
-        message += f"\nСвяжитесь с администратором: {admin_contact}"
 
     await update.message.reply_text(message)
     context.user_data["state"] = States.MAIN_MENU
@@ -70,5 +65,31 @@ async def bug_report(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Stat
 
 async def feature_report(update: Update, context: ContextTypes.DEFAULT_TYPE) -> States:
     logger.info(f"Feature report: {update.message.text}")
+    text = " ".join(context.args) if context.args else None
+    if text is None:
+        await update.message.reply_text(
+            "Предложите пожалуйста свою идею в одном сообщении:"
+        )
+        context.user_data["state"] = States.FEATURE_REPORT_WAIT
+        return States.FEATURE_REPORT_WAIT
 
-    return States.FEATURE_REPORT_WAIT
+    message = await register_report(
+        telegram_id=update.effective_user.id, text=text, type_report=TypeReport.FEATURE
+    )
+
+    await update.message.reply_text(message)
+    context.user_data["state"] = States.MAIN_MENU
+    return States.MAIN_MENU
+
+
+async def register_report(telegram_id: int, text: str, type_report: TypeReport) -> str:
+    user = get_user_by_telegram_id(telegram_id)
+    success, message = await ReportHandler.handle_report(
+        user=user, text=text, source=SourceReports.TELEGRAM, type_report=type_report
+    )
+
+    if not success:
+        admin_contact = await AdminNotifier.get_admin_contacts()
+        message += f"\nСвяжитесь с администратором: {admin_contact}"
+
+    return message
