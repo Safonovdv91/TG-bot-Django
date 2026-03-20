209a0c5ee4aabb500a94cd6a761921fb17fc15bf
Safonov Dmitry (mac) <Softik.d@gmail.com>
2026-03-20 20:45:20 +0300

    BugFIX: fix отображения дебаг режима

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,10 +1,15 @@
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
+
+from core.settings import DEBUG
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("ggp/", include("gymkhanagp.urls", namespace="gymkhanagp")),
     path("", include("index.urls", namespace="index")),
-] + debug_toolbar_urls()
+]
+if DEBUG:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+    urlpatterns += debug_toolbar_urls()
+
